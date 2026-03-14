9c8a29941de60d9a37d7f03ba798e95f26f63817
Chandrashekar Babu <chandra@Telesto.local>
2026-03-15 02:17:14 +0530

    Another test commit

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -31,7 +31,7 @@ export function Hero() {
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ duration: 0.8, delay: 0.2 }}
               >
-                THE TURING CHANDRA SHEKAR BABU
+                THE TURING 
                 <br />
                 THRESHOLD
               </motion.h1>
